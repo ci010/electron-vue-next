7ed189e1f3d1a4b86ee94b580d8e2c8fc0ae55c4
CI010 <cijhn@hotmail.com>
2020-10-22 01:08:04 +0800

    chore: set workflow for test

--- .github/actions/get-build-number/index.js
+++ .github/actions/get-build-number/index.js
@@ -1,10 +1,9 @@
-const core = require('@actions/core');
-
 async function main(output) {
    output('build_number', process.env.GITHUB_RUN_NUMBER);
 }
 
-main(core ? core.setOutput : (k, v) => {
-    console.log(k)
-    console.log(v)
-});
+function setOutput(name, value) {
+    process.stdout.write(Buffer.from(`::set-output name=${name}::${value}`))
+}
+
+main(setOutput);
