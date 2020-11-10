2dd79828634dfc146c614118c2032cb80587d06c
Lyman Lai <lymanlai@users.noreply.github.com>
2020-11-11 01:07:00 +0800

    fix: 👷 fix build error chalk default not found (#20)
    Fix the error: TypeError: Cannot read property 'bold' of undefined
    
    Co-authored-by: Lyman Lai <lyman@yaha.me>

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,6 @@
 const { join } = require('path')
 const { build } = require('vite')
-const chalk = require('chalk').default
+const chalk = require('chalk')
 const { build: electronBuilder } = require('electron-builder')
 const { stat, remove, copy } = require('fs-extra')
 const { rollup } = require('rollup')
@@ -9,13 +9,19 @@ const loadConfigFile = require('rollup/dist/loadConfigFile')
 /**
  * Use typescript to build main process
  */
-async function buildMain() {
-  await Promise.all([remove(join(__dirname, '../dist/electron/index.dev.js')), remove(join(__dirname, '../dist/electron/index.dev.js.map'))])
+async function buildMain () {
+  await Promise.all([
+    remove(join(__dirname, '../dist/electron/index.dev.js')),
+    remove(join(__dirname, '../dist/electron/index.dev.js.map'))
+  ])
   const start = Date.now()
   console.log(chalk.bold.underline('Build main process'))
-  const { options, warnings } = await loadConfigFile(join(__dirname, 'rollup.config.js'), {
-    input: join(__dirname, '../src/main/index.prod.ts')
-  })
+  const { options, warnings } = await loadConfigFile(
+    join(__dirname, 'rollup.config.js'),
+    {
+      input: join(__dirname, '../src/main/index.prod.ts')
+    }
+  )
   warnings.flush()
   /**
    * @type {import('rollup').RollupOptions}
@@ -30,17 +36,23 @@ async function buildMain() {
     if (chunk.type === 'chunk') {
       const filepath = join('dist', 'electron', chunk.fileName)
       const { size } = await stat(join(__dirname, '..', filepath))
-      console.log(`${chalk.gray('[write]')} ${chalk.cyan(filepath)}  ${(size / 1024).toFixed(2)}kb`)
+      console.log(
+        `${chalk.gray('[write]')} ${chalk.cyan(filepath)}  ${(
+          size / 1024
+        ).toFixed(2)}kb`
+      )
     }
   }
-  console.log(`Build completed in ${((Date.now() - start) / 1000).toFixed(2)}s.`)
+  console.log(
+    `Build completed in ${((Date.now() - start) / 1000).toFixed(2)}s.`
+  )
   console.log()
 }
 
 /**
  * Use vite to build renderer process
  */
-async function buildRenderer() {
+async function buildRenderer () {
   const config = require('./vite.config')
   console.log(chalk.bold.underline('Build renderer process'))
   await build({
@@ -57,28 +69,39 @@ async function buildRenderer() {
  * @param {import('electron-builder').Configuration} config The electron builder config
  * @param {boolean} dir Use dir mode to build
  */
-async function buildElectron(config, dir) {
+async function buildElectron (config, dir) {
   console.log(chalk.bold.underline('Build electron'))
   const start = Date.now()
-  await electronBuilder({ publish: 'never', config, dir }).then(async (files) => {
+  await electronBuilder({ publish: 'never', config, dir }).then(async files => {
     for (const file of files) {
       const fstat = await stat(file)
-      console.log(`${chalk.gray('[write]')} ${chalk.yellow(file)} ${(fstat.size / 1024 / 1024).toFixed(2)}mb`)
+      console.log(
+        `${chalk.gray('[write]')} ${chalk.yellow(file)} ${(
+          fstat.size /
+          1024 /
+          1024
+        ).toFixed(2)}mb`
+      )
     }
   })
-  console.log(`Build completed in ${((Date.now() - start) / 1000).toFixed(2)}s.`)
+  console.log(
+    `Build completed in ${((Date.now() - start) / 1000).toFixed(2)}s.`
+  )
 }
 
-async function copyStatic() {
+async function copyStatic () {
   await remove(join(__dirname, '../dist/electron/static'))
-  await copy(join(__dirname, '../static'), join(__dirname, '../dist/electron/static'))
+  await copy(
+    join(__dirname, '../static'),
+    join(__dirname, '../dist/electron/static')
+  )
 }
 
-async function start() {
+async function start () {
   /**
    * Load electron-builder Configuration
    */
-  function loadConfig() {
+  function loadConfig () {
     switch (process.env.BUILD_TARGET) {
       case 'production':
         return require('./build.config')
