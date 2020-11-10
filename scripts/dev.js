486632f5564cefbf38df40c1a8d2224b26f6f07e
Alex Kozack <kozackunisoft@gmail.com>
2020-11-10 10:11:38 +0200

    Refactoring /scripts/dev.js

--- scripts/dev.js
+++ scripts/dev.js
@@ -9,33 +9,19 @@ const { EOL } = require('os')
 
 const manualRestart = false
 
-const child = {
-  /**
-   * @type {import('child_process').ChildProcessWithoutNullStreams  | null}
-   */
-  electron: null,
-
-  /**
-   * @type {import('http').Server  | null}
-   */
-  viteServer: null,
-
-  /**
-   * @type {import('rollup').RollupWatcher  | null}
-   */
-  rollupWatcher: null
-}
+/**
+ * @type {import('child_process').ChildProcessWithoutNullStreams  | null}
+ */
+let electronProcess = null
 
 /**
  * Start electron process and inspect port 5858 with 9222 as debug port.
  */
 function startElectron() {
-  /**
-   * @type {any}
-   */
-  const electronCommand = electron
-  child.electron = spawn(electronCommand,
-    ['--inspect=5858', '--remote-debugging-port=9222', join(__dirname, '../dist/electron/index.dev.js')])
+  electronProcess = spawn(
+    /** @type {string} */electron,
+    ['--inspect=5858', '--remote-debugging-port=9222', join(__dirname, '../dist/electron/index.dev.js')]
+  )
 
   /**
    * @param {string | Buffer} data
@@ -58,16 +44,17 @@ function startElectron() {
         .map(colorize).join(EOL)
     )
   }
-  child.electron.stdout.on('data', electronLog)
-  child.electron.stderr.on('data', electronLog)
+  electronProcess.stdout.on('data', electronLog)
+  electronProcess.stderr.on('data', electronLog)
 
-  child.electron.on('close', (code, sig) => {
+  electronProcess.on('exit', (code, sig) => {
     if (!manualRestart) {
       // if (!devtoolProcess.killed) {
       //     devtoolProcess.kill(0);
       // }
+
       if (!sig) { // Manual close
-        stop()
+        process.exit(0)
       }
     }
   })
@@ -77,8 +64,8 @@ function startElectron() {
  * Kill and restart electron process
  */
 function reloadElectron() {
-  if (child.electron) {
-    child.electron.kill('SIGTERM')
+  if (electronProcess) {
+    electronProcess.kill('SIGTERM')
     console.log(chalk.bold.underline.green('Electron App Restarted'))
   } else {
     console.log(chalk.bold.underline.green('Electron App Started'))
@@ -89,9 +76,9 @@ function reloadElectron() {
 /**
  * Start vite dev server for renderer process and listen 8080 port
  */
-async function startRenderer() {
+function startRenderer() {
   const config = require('./vite.config')
-  child.viteServer = await createServer(config).listen(8080)
+  return createServer(config).listen(8080)
 }
 
 async function startMain() {
@@ -99,54 +86,40 @@ async function startMain() {
     input: join(__dirname, '../src/main/index.dev.ts'),
     sourcemap: true
   })
+
   warnings.flush()
+
   /**
    * @type {import('rollup').RollupOptions}
    */
   const config = options[0]
-  child.rollupWatcher = watch({
+  watch({
     ...config,
     watch: {
       buildDelay: 500
     }
   })
+    .on('change', (id) => console.log(`${chalk.grey('[change]')} ${id}`))
 
-  child.rollupWatcher.on('event', (event) => {
-    switch (event.code) {
-      case 'END':
-        reloadElectron()
-        break
-      case 'BUNDLE_END':
-        console.log(`${chalk.grey('[bundle]')} ${event.output} ${event.duration + 'ms'}`)
-        break
-      case 'ERROR':
-        console.error(event.error)
-        break
-    }
-  }).on('change', (id) => {
-    console.log(`${chalk.grey('[change]')} ${id}`)
-  })
-}
-
-function stop(code = 0) {
-  if (child.electron) {
-    child.electron.kill()
-  }
-
-  if (child.viteServer) {
-    child.viteServer.removeAllListeners()
-    child.viteServer.close()
-  }
-
-  if (child.rollupWatcher) {
-    child.rollupWatcher.removeAllListeners()
-    child.rollupWatcher.close()
-  }
-
-  process.exit(code)
+    .on('event', (event) => {
+      switch (event.code) {
+        case 'END':
+          reloadElectron()
+          break
+        case 'BUNDLE_END':
+          console.log(`${chalk.grey('[bundle]')} ${event.output} ${event.duration + 'ms'}`)
+          break
+        case 'ERROR':
+          console.error(event.error)
+          break
+      }
+    })
 }
 
 Promise.all([
   startMain(),
   startRenderer()
-]).catch(console.error)
+]).catch(e => {
+  console.error(e)
+  process.exit(0)
+})
