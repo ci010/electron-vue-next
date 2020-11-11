d7ebcc5f877695e0a23e14c6342909b4c080e2cb
ci010 <cijhn@hotmail.com>
2020-11-11 16:26:45 +0800

    build doc

--- scripts/dev.docs.js
+++ scripts/dev.docs.js
@@ -2,19 +2,22 @@ const chalk = require('chalk')
 const { createServer } = require('vitepress')
 const { excludeOptimize } = require('../package.json')
 
+const port = 3000
+
 async function startVitepress() {
-  createServer({
-    root: 'docs',
-    optimizeDeps: {
-      exclude: excludeOptimize
-    },
-  }).then((server) => {
-    server.listen(3000, () => {
-      console.log(`listening at http://localhost:${3000}`)
+  try {
+    const server = await createServer({
+      root: 'docs',
+      optimizeDeps: {
+        exclude: excludeOptimize
+      }
     })
-  }).catch((err) => {
+    server.listen(port, () => {
+      console.log(`${chalk.green('[vitepress]')} listening at http://localhost:${port}`)
+    })
+  } catch (err) {
     console.error(chalk.red('failed to start server. error:\n'), err)
-  })
+  }
 }
 
 startVitepress()
