59d5b2ab199296b96135d8bc47281756c3a74321
ci010 <cijhn@hotmail.com>
2021-06-22 13:50:24 +0800

    refactor: remove vetur, add volar, and remove sample directive

--- electron-vue-next/src/renderer/App.vue
+++ electron-vue-next/src/renderer/App.vue
@@ -7,16 +7,12 @@
 
 <script lang=ts>
 import { defineComponent } from 'vue'
-import directive from '/@/directives/sample'
 import Navigator from '/@/components/HomeNavigator.vue'
 
 export default defineComponent({
   components: {
     Navigator
   },
-  directives: {
-    directive
-  }
 })
 </script>
 
--- vetur.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('vls').VeturConfig} */
-const config = {
-  projects: [{
-    root: './electron-vue-next/src/renderer',
-    tsconfig: './tsconfig.json'
-  }]
-}
-
-module.exports = config
